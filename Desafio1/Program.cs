448ec64778e50d8cb81758778edf91c406da813d
Facu Neffa <facundoneffa@gmail.com>
2024-01-18 17:49:26 -0300

    primera subida

---
+++ Desafio1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido al catalogo telefonico");
+            Console.ReadKey();
+        
+            Telefono tf= new Telefono("S23", "Samsung");
+            tf.NumeroTelefonico = "Como estas papu?";
+            
+            Console.WriteLine(tf.Llamar());
+            Console.ReadKey();
+            string contacto = "Amigo";
+            Console.WriteLine(tf.Llamar(contacto));
+            Console.ReadKey();
+            Console.WriteLine(tf.EnviarMensaje(tf.NumeroTelefonico, "Hola CARLOS"));
+            Console.ReadKey();
+
+            
+
+
+
+        }
+    }
+}
---
+++ Desafio1/Telefono.cs
@@ -0,0 +1,49 @@
+﻿namespace Desafio1
+{
+    internal class Telefono
+    {
+        public string Modelo { get; }
+        public string Marca { get; }
+        public string NumeroTelefonico { get; set; }
+        private int codigoOperador;
+
+        public int destino { get; set; }
+
+        public int CodigoOperador
+        {
+            get { return codigoOperador; }
+            set
+            {
+                if (value >= 1 && value <= 3)
+                {
+                    codigoOperador = value;
+                }
+                else
+                {
+                    codigoOperador = 0;
+                }
+            }
+        }
+
+        public Telefono(string modelo, string marca)
+        {
+            Modelo = modelo;
+            Marca = marca;
+        }
+
+        public string Llamar()
+        {
+            return "Realizando llamada..." + NumeroTelefonico;
+        }
+
+        public string Llamar(string contacto)
+        {
+            return "Llamando a " + contacto;
+        }
+
+        public string EnviarMensaje(string mensaje, string destino)
+        {
+            return "Enviando mensaje a... " + destino + " : "+ mensaje;
+        }
+    }
+}
